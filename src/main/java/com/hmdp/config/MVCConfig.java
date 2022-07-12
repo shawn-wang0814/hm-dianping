3b138adc12e098e9ff6a6f2618c57d4e0df2b169
Jackson <szxxwang@example.com>
2022-07-12 16:50:58 +0800

    hmdp project login function not finished.

---
+++ src/main/java/com/hmdp/config/MVCConfig.java
@@ -0,0 +1,27 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Project Name:hm-dianping
+ * File Name:null.java
+ * Package Name:com.hmdp.config
+ * Date:2022/7/12 16:29
+ * Copyright (c) 2022, szxxwang@outlook.com All Rights Reserved.
+ */
+public class MVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
+                "/shot-type/**",
+                "/shop/**",
+                "/upload/**",
+                "/blog/hot",
+                "/user/code",
+                "/user/login"
+        );
+
+    }
+}
